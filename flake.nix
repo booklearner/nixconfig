da0677bfefa51266bcacbb86215788a7a6a9f410
booklearner <booklearner@protonmail.com>
2022-05-09 12:03:51 -0400

    add inputs for home manager and pkgs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
-  description = "A very basic flake";
+  description = "booklearner's nix configuration flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixpkgs-21.11-darwin";
+    
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+  outputs = inputs@{ nixpkgs, nixpkgs-stable, home-manager, ... }: {
 
   };
 }
